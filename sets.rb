d0d1f3c02b1fbb46af9fe17f123a26e18f9a9ed4
isalevine <isa.loves.coding@gmail.com>
2019-10-13 12:31:06 -0700

    commented out part 2-object lookup code

--- sets.rb
+++ sets.rb
@@ -7,7 +7,7 @@
 
 # string = "When you choose this domain at 1st level, you gain proficiency with heavy armor."
 
-# regex1 = /(-)|(--)|(\.\.\.)|(_)/          # separate words joined by hyphens, m-dashes, underscores, and elipses
+# regex1 = /(-)|(--)|(\.\.\.)|(_)/          # separate words joined by hyphens, m-dashes, underscores, and ellipses
 # regex2 = /([.,:;?!"'`@#$%^&*()+={}-])/    # remove all other punctuation
 # string.gsub!(regex1, " ")
 # string.gsub!(regex2, "")
@@ -22,26 +22,33 @@ require 'set'
 
 string = "when you choose this domain at 1st level you gain proficiency with heavy armor"
 
-# array = string.split(" ")
-# set = Set.new(array)
-
-# puts set
-
-
-
-filter_words = Set["a", "an", "the", "and", "is", "of", "to", "be", "in", "they", "their", 
-    "them", "or", "if", "this", "like", "had", "but", "what", "with", "at",
-]
-
 array = string.split(" ")
+set = Set.new(array)
 
-set = Set.new
-array.each do |str|
-    if !filter_words.include?(str)
-        set.add(str)
-    end
+set.each do |str|
+    puts str
 end
 
-puts set
+
+
+
+
+# part 2: object lookup
+# =====================
+
+# filter_words = Set["a", "an", "the", "and", "is", "of", "to", "be", "in", "they", "their", 
+#     "them", "or", "if", "this", "like", "had", "but", "what", "with", "at",
+# ]
+
+# array = string.split(" ")
+
+# set = Set.new
+# array.each do |str|
+#     if !filter_words.include?(str)
+#         set.add(str)
+#     end
+# end
+
+# puts set
 
 
