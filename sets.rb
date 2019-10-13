772fcc7e418c73a675206f3972fdc01cc8b05ab8
isalevine <isa.loves.coding@gmail.com>
2019-10-13 11:03:24 -0700

    added string and regex gsubs from FCG's seeds.rb file

---
+++ sets.rb
@@ -0,0 +1,19 @@
+require 'set'
+
+string = "When you choose this domain at 1st level, you gain proficiency with heavy armor."
+
+regex1 = /(-)|(--)|(\.\.\.)|(_)/
+regex2 = /([.,:;?!"'`@#$%^&*()+={}-])/
+
+string.downcase!
+string.gsub!(regex1, " ")
+string.gsub!(regex2, "")
+array = string.split(" ")
+set = Set.new(array)
+
+
+
+
+# filter_words = ["a", "an", "the", "and", "is", "of", "to", "be", "in", "they", "their", 
+#     "them", "or", "if", "this", "like", "had", "but", "what", "with", "at",
+# ]
