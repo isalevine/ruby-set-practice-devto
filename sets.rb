31cc70b8624647d5ca8238c543caa19f0677c5cb
isalevine <isa.loves.coding@gmail.com>
2019-10-13 11:39:06 -0700

    added several variations of the code

--- sets.rb
+++ sets.rb
@@ -1,22 +1,47 @@
 # originally copied/based on Friendly Character Generator backend-api's seeds.rb file
 
 
-require 'set'
+
+# for text normalization demo:
+# ============================
 
 string = "When you choose this domain at 1st level, you gain proficiency with heavy armor."
 
-regex1 = /(-)|(--)|(\.\.\.)|(_)/
-regex2 = /([.,:;?!"'`@#$%^&*()+={}-])/
-
+regex1 = /(-)|(--)|(\.\.\.)|(_)/        # separate words joined by hyphens, m-dashes, underscores, and elipses
+regex2 = /([.,:;?!"'`@#$%^&*()+={}-])/  # remove all other punctuation
 string.downcase!
 string.gsub!(regex1, " ")
 string.gsub!(regex2, "")
+
+puts string
+#=> "when you choose this domain at 1st level you gain proficiency with heavy armor"
+
+
+
+require 'set'
+
+string = "when you choose this domain at 1st level you gain proficiency with heavy armor"
+
+# array = string.split(" ")
+# set = Set.new(array)
+
+# puts set
+
+
+
+filter_words = Set["a", "an", "the", "and", "is", "of", "to", "be", "in", "they", "their", 
+    "them", "or", "if", "this", "like", "had", "but", "what", "with", "at",
+]
+
 array = string.split(" ")
-set = Set.new(array)
+
+set = Set.new
+array.each do |str|
+    if !filter_words.include?(str)
+        set.add(str)
+    end
+end
+
+puts set
 
 
-
-
-# filter_words = ["a", "an", "the", "and", "is", "of", "to", "be", "in", "they", "their", 
-#     "them", "or", "if", "this", "like", "had", "but", "what", "with", "at",
-# ]
