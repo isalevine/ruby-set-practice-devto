350a6638a9da410b88c6af946c281c59e97df3c9
isalevine <isa.loves.coding@gmail.com>
2019-11-03 11:11:42 -0800

    final code for article - mixed_sorted_set commented out

--- sortedsets.rb
+++ sortedsets.rb
@@ -2,22 +2,22 @@ require 'set'
 
 str_array = ["i", "am", "watching", "the", "office", "right", "now", "now", "now"]
 int_array = [2, 6, 1, 4, 7, 2, 3, 3, 3, 1, 0, 5]
-mixed_array = [2, "i", 6, "am", 1, "watching", 4, "the", 7, "office", 2]
+# mixed_array = [2, "i", 6, "am", 1, "watching", 4, "the", 7, "office", 2]
 
 
 
-# str_sorted_set = SortedSet.new(str_array)
-# int_sorted_set = SortedSet.new(int_array)
+str_sorted_set = SortedSet.new(str_array)
+int_sorted_set = SortedSet.new(int_array)
 # mixed_sorted_set = SortedSet.new(mixed_array)
 
 
 
-# str_set = Set.new(str_array)
-# int_set = Set.new(int_array)
+str_set = Set.new(str_array)
+int_set = Set.new(int_array)
 # mixed_set = Set.new(mixed_array)
 
-# str_sorted_set = SortedSet.new(str_set)
-# int_sorted_set = SortedSet.new(int_set)
+str_sorted_set = SortedSet.new(str_set)
+int_sorted_set = SortedSet.new(int_set)
 # mixed_sorted_set = SortedSet.new(mixed_set)
 
 
